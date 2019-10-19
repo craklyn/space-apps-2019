fda8ada85c085566961b07ea6b324490ad7aca11
Paris Morgan <pmorgan@microsoft.com>
2019-10-19 16:43:07 -0700

    website up

---
+++ beautiful_earth/application.py
@@ -0,0 +1,3 @@
+from app import app
+
+# Note - this file is named application.py intentionally. See: https://docs.microsoft.com/en-us/azure/python/tutorial-deploy-app-service-on-linux-04#flask-startup-commands
